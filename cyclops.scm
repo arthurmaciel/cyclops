9a16ef69a60c13825c1c86939ee255ce1cd822da
Justin Ethier <justin.ethier@gmail.com>
2017-03-05 21:45:38 +0000

    Added (split)

--- cyclops.scm
+++ cyclops.scm
@@ -4,12 +4,36 @@
   (scheme write)
   (scheme file)
   (scheme cyclone libraries)
+  (scheme cyclone transforms)
+  (scheme cyclone util)
 )
 
 ;; TODO: constants to later externalize
 (define *cyclone-repo-dir* ".") ;; TODO: temporarily use local for testing
 (define *pkg-file-dir* "../cyclone-packages/sample-lib")
 (define *pkg-file* "../cyclone-packages/sample-lib/package.scm")
+
+;; split :: string -> char -> [string]
+;; Take the given string and split it into substrings separated by delim.
+;; Delim must be a character and is not included in the substrings.
+(define (split str delim)
+  (filter 
+    (lambda (s)
+      (> (string-length s) 0))
+    (reverse
+      (map
+        (lambda (lis)
+          (list->string (reverse lis)))
+        (foldl 
+          (lambda (chr accum)
+            (if (eq? chr delim)
+              (cons '() accum)
+              (cons 
+                (cons chr (car accum))
+                (cdr accum)))
+          )
+          '(())
+          (string->list str))))))
 
 ;; Read given file and determine if it is a library
 (define (file-is-library? filename)
@@ -45,7 +69,8 @@
   (for-each
     (lambda (file)
       (let* ((file/path (string-append *pkg-file-dir* "/" file))
-             (lib? (file-is-library? file/path)))
+             (lib? (file-is-library? file/path))
+             (file/path-no-ext (basename file/path)))
 (write `(,file/path ,(file-is-library? file/path)))(newline) ;; DEBUG
         (system 
           (string-append 
@@ -58,17 +83,35 @@
           (set! install-file-list 
              (append 
                install-file-list 
-               (list file/path)))) ;; TODO: .o .meta files. possibly any included .scm files, too
+               (list 
+                file/path
+                (string-append file/path-no-ext ".o")
+                (string-append file/path-no-ext ".meta")
+               )))) ;; TODO: possibly any included .scm files, too
         (else
           (set! install-file-list 
              (append 
                install-file-list 
-               (list file/path))))) ;; TODO: compiled executable
+               (list 
+                file/path-no-ext ;; compiled executable
+               )))))
     ))
     (get-file-list params))
 
   ;; TODO: Install files
-(write `(DEBUG ,install-file-list)) (newline) ;; more DEBUGGING
+(write `(files to install: ,install-file-list)) (newline) ;; more DEBUGGING
+  (for-each
+    (lambda (filename)
+      ; Strip off leading directory
+      (let* ((pkg-basedir-len (string-length *pkg-file-dir*))
+             (fn-no-base (substring filename pkg-basedir-len (string-length filename))))
+        (write `(DEBUG ,fn-no-base))
+      )
+
+      ;(system 
+      ;  (string-append "cp " filename " " *cyclone-repo-dir*))
+    )
+    install-file-list)
 
   ;; Run the 'install' section, if applicable 
   (let ((install-directive (assoc 'install params)))
@@ -79,8 +122,11 @@
           (cdr install-directive))))
   )
 
-(call-with-input-file 
+#;(call-with-input-file 
   *pkg-file*
   (lambda (fp)
     (read-pkg fp)
   ))
+
+(write
+  (split "/scheme/cyclone/sample.sld" #\/))
