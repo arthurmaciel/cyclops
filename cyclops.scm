23a7513e8eed00c76339d793d277cc8fcf51ac2b
Justin Ethier <justin.ethier@gmail.com>
2017-03-08 11:48:38 +0000

    Added a command line interface

--- cyclops.scm
+++ cyclops.scm
@@ -1,9 +1,4 @@
 ;; Roadmap:
-;- consider, may be more flexible to have a build() target that puts everything in a sub-directory and overlays that to install, vs separate file/install/uninstall sections. look a PKGBUILD for an example: https://github.com/justinethier/cyclone-bootstrap/blob/master/dist/PKGBUILD
-;  YES, will want to switch over to this. will be more work up front but should simplify things. can think through the design a bit before implementing anything
-;  will probably need a means of passing the destination dir as a variable to in the package.scm file's package section.
-;  may need to document the set of predefined variables
-;
 ;- get install, test, uninstall working locally
 ;- modularize code, add a command line interface
 ;- add a concept of a local repo "db" to keep track of:
@@ -117,11 +112,12 @@
 (define (read-pkg fp cmd)
   (let ((params (read-all fp)))
     (cond
-      ((eq? cmd 'install)
-       (install params))
-      ((eq? cmd 'uninstall)
-       (uninstall params))
-      ((eq? cmd 'test)
+      ((equal? cmd "install")
+       (run-directive params 'build)
+       (run-directive params 'install))
+      ((equal? cmd "uninstall")
+       (run-directive params 'uninstall))
+      ((equal? cmd "test")
        (run-directive params cmd))
       (else
        (error "Unsupported command" cmd)))))
@@ -141,111 +137,6 @@
               (else
                 (error "Unsupported filename" filename))))
           (cdr files)))))
-
-;; install :: alist -> void
-;; Build & Install files using the given package file parameters
-(define (install params)
-;  (define install-file-list '())
-;  ;; Build files
-;  (for-each
-;    (lambda (file)
-;      (let* ((file/path (string-append *pkg-file-dir* "/" file))
-;             (lib? (file-is-library? file/path))
-;             (file/path-no-ext (basename file/path)))
-;(write `(,file/path ,(file-is-library? file/path)))(newline) ;; DEBUG
-;        (run-sys-cmd
-;          "cyclone " 
-;          file/path)
-;      ;; TODO: check return code, make sure build succeeded
-;      ;; TODO: if library, install .o .sld .meta files
-;      (cond
-;        (lib?
-;          (set! install-file-list 
-;             (append 
-;               install-file-list 
-;               (map
-;                 (lambda (file)
-;                   (cons *cyclone-repo-lib-dir* file))
-;                 (list 
-;                  file/path
-;                  (string-append file/path-no-ext ".o")
-;                  (string-append file/path-no-ext ".meta")
-;                 ))))) ;; TODO: possibly any included .scm files, too
-;        (else
-;          (set! install-file-list 
-;             (append 
-;               install-file-list 
-;               (list 
-;                (cons *cyclone-repo-bin-dir* file/path-no-ext) ;; compiled executable
-;               )))))
-;    ))
-;    (get-file-list params))
-;
-;  ;; Install files
-;  ;(write `(files to install: ,install-file-list)) (newline) ;; more DEBUGGING
-;  (if (not (file-exists? *cyclone-repo-bin-dir*))
-;      (run-sys-cmd "mkdir " *cyclone-repo-bin-dir*))
-;  (if (not (file-exists? *cyclone-repo-lib-dir*))
-;      (run-sys-cmd "mkdir " *cyclone-repo-lib-dir*))
-;  (for-each
-;    (lambda (dest/filename)
-;      ; Strip off leading directory
-;      (let* ((dest (car dest/filename))
-;             (filename (cdr dest/filename))
-;             (pkg-basedir-len (string-length *pkg-file-dir*))
-;             (fn-no-base (substring filename pkg-basedir-len (string-length filename))))
-;        (create-missing-dirs fn-no-base dest)
-;        (run-sys-cmd "cp " filename " " dest "/" fn-no-base)
-;      ))
-;    install-file-list)
-
-  (run-directive params 'build)
-  ;; Run the 'install' section, if applicable 
-  (run-directive params 'install)
-  )
-
-(define (uninstall params)
-;  (define file-list '())
-;  ;; Build up the list of installed files
-;  (for-each
-;    (lambda (file)
-;      (let* ((file/path (string-append *pkg-file-dir* "/" file))
-;             (lib? (file-is-library? file/path))
-;             (file/path-no-ext (basename file/path)))
-;    (write `(,file/path ,(file-is-library? file/path)))(newline) ;; DEBUG
-;      (cond
-;        (lib?
-;          (set! file-list 
-;             (append 
-;               file-list 
-;               (map
-;                 (lambda (file)
-;                   (cons *cyclone-repo-lib-dir* file))
-;                 (list 
-;                  file/path
-;                  (string-append file/path-no-ext ".o")
-;                  (string-append file/path-no-ext ".meta")
-;                 ))))) ;; TODO: possibly any included .scm files, too
-;        (else
-;          (set! file-list 
-;             (append 
-;               file-list 
-;               (list 
-;                (cons *cyclone-repo-bin-dir* file/path-no-ext) ;; compiled executable
-;               )))))))
-;    (get-file-list params))
-;(write `(uninstall list ,file-list))(newline)
-;;  ;; Uninstall each file in file-list
-;  (for-each
-;    (lambda (dest/filename)
-;      ;; TODO: delete the installed files, leave dirs (at least for now)
-;      (write `(TODO uninstall ,dest/filename))(newline)
-;    )
-;    file-list)
-
-  ;; Run the 'uninstall' section, if applicable 
-  (run-directive params 'uninstall)
-)
 
 ;; create-missing-dirs :: string -> string -> void
 ;; Accept file with a path destination, and create any missing directories
@@ -268,18 +159,38 @@
             (run-sys-cmd "mkdir " dir)))
        path)))
 
+(let* ((args (command-line-arguments))
+      )
+  (cond
+    ((or (null? args)
+         (< (length args) 2))
+     (write "TODO: usage")
+     (newline)
+     (exit 1))
+    (else
+      (let ((cmd (car args))
+            (pkgfile (cadr args)))
+        (write `(pkg dir ,(basename pkgfile)))
+        (call-with-input-file
+          pkgfile
+          (lambda (fp)
+            (read-pkg fp cmd))))
+      )))
+;; TODO: create a cyclone library for command line arguments??
+
+
 ;(write (chdir *pkg-file-dir*))
 ;(write (getcwd))
-(call-with-input-file 
-  *pkg-file*
-  (lambda (fp)
-    (read-pkg fp 'install)
-    ;(read-pkg fp 'uninstall)
-    ;(read-pkg fp 'test)
-  ))
-
-#;(write
-  (split "/scheme/cyclone/sample.sld" #\/))
-
-#;(write
-  (create-missing-dirs "scheme/cyclone/sample.sld" "."))
+;(call-with-input-file 
+;  *pkg-file*
+;  (lambda (fp)
+;    ;(read-pkg fp 'install)
+;    (read-pkg fp 'uninstall)
+;    ;(read-pkg fp 'test)
+;  ))
+;
+;#;(write
+;  (split "/scheme/cyclone/sample.sld" #\/))
+;
+;#;(write
+;  (create-missing-dirs "scheme/cyclone/sample.sld" "."))
